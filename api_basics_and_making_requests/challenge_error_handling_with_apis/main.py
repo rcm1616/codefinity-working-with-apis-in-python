ecfa4a69a59f12a1144a29c45752dfae609b2377
rcm1616 <>
2026-04-24 07:08:58 +0000

    Challenge: Error Handling with APIs

---
+++ api_basics_and_making_requests/challenge_error_handling_with_apis/main.py
@@ -0,0 +1,14 @@
+import requests
+
+def get_cat_fact():
+    url = "https://catfact.ninja/fact"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.status_code)
+        data = response.json()
+        print(data["fact"])
+    except Exception:
+        print("Failed to retrieve cat fact.")
+
+get_cat_fact()
